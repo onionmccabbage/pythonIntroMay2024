bcfdb8707af90c8c1bdff176ed90b0e0480e8410
onionmccabbage <onionmccabbage@live.com>
2024-05-29 10:45:08 +0100

    basic function

---
+++ structure.py
@@ -0,0 +1,9 @@
+# how to structure python modules
+# using functions, loops, conditionals, __main__
+def myFn(): # the colon indicates the start of a code block
+    return 'hello'    # every indented line belongs to this code block
+
+# when the lines are no longer indented, the code block has ended
+
+result = myFn() # we invoke the function
+print(result)
