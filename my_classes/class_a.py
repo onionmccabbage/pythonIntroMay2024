7ddc2e391d186d2ca57ad7b1d42a93e9fe922ebf
onionmccabbage <onionmccabbage@live.com>
2024-05-29 15:32:33 +0100

    simple class

---
+++ my_classes/class_a.py
@@ -0,0 +1,20 @@
+# we can declare as many classes as we like in Python
+
+# here is a demo
+myPoint_1 = {'x':3, 'y':4} # here we have x and y
+myPoint_2 = (4,3)
+myPoint_3 = [4,3]
+# none of the above let me validate the values of x and y
+myPoint_1['x'] = 'wibble'
+myPoint_3[0] = True
+
+class Point2d(): # the brackets are optional
+    '''Here we encapsulate a geometric point in 2-d space
+    x and y will be floating point values (no defaults)'''
+    def __init__(self, x, y): # every class method MUST take 'self' as an argument
+        '''this is the initialiser for the class. 
+        It is run every time we make a new instance'''
+        print(self)
+
+if __name__ == '__main__':
+    p1 = Point2d(3,4) # we now have an instance of our class (calls the __init__ method)
