e5dd8c3ccd6077b0d7d2c1b317fac6b09b7b2ed0
onionmccabbage <onionmccabbage@live.com>
2024-05-29 15:49:18 +0100

    using getter and setter decorators

--- my_classes/class_a.py
+++ my_classes/class_a.py
@@ -15,17 +15,30 @@ class Point2d(): # the brackets are optional
         '''this is the initialiser for the class. 
         It is run every time we make a new instance'''
         # print(self)
-        self.__x = x # we use __x to 'mangle' the access to x
-        self.__y = y # mangling prevents direct acces to the values
+        self.x = x # we call the setter for x (and use mangling)
+        self.y = y # mangling prevents direct acces to the values
     # to provide access to these mangled properties, we write 'getter' and 'setter' methods
     # also known as accessor and mutator methods
     @property # this is a decorator (note the @ sign)
     def x(self):
-        return self.__x
-    
-    @property
+        return self.__x # we may do anything but here we just return the mangled value
+    @x.setter # this decorator is the 'setter' or 'mutator' method for x
+    def x(self, new_x):
+        '''we can validate the incoming value before setting'''
+        if type(new_x) in (int, float):
+            self.__x = new_x
+        else:
+            self.__x = 0 # we may choose to set a default
+    @property # this is the getter or the accessor for the property
     def y(self):
         return self.__y
+    @y.setter
+    def y(self, new_y):
+        '''validate y is eiher int or float'''
+        if type(new_y) in (int, float):
+            self.__y = new_y
+        else:
+            raise TypeError # we may choose ot raise an exception
 
 if __name__ == '__main__':
     p1 = Point2d(3,4) # we now have an instance of our class (calls the __init__ method)
