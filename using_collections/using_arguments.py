c0a25fd78266290ac4b74667fe237676c00b7823
onionmccabbage <onionmccabbage@live.com>
2024-05-30 10:37:42 +0100

    passing keyword arguments into a dict

--- using_collections/using_arguments.py
+++ using_collections/using_arguments.py
@@ -15,9 +15,17 @@ def fnA(*args): # the asterisk * lets us gather all positional arguments togethe
         return args[0]*args[1]*args[2]
     return # in all other cases do nothing
 
+def fnB(**kwargs): # **kwargs will gather all keyword arguments into a dict
+    '''all the keyword arguments will be gathered into a dict'''
+    r = ''
+    for (k,v) in kwargs.items():
+        r = r+f'Key {k} contains {v}\n'
+    return r
+
 
 if __name__ == '__main__':
-    fnA(2.345) # one argument
-    fnA(4,5)   # two arguments
-    fnA(7,8,9) # three arguments
+    print( fnA(2.345) ) # one argument
+    print( fnA(4,5) )   # two arguments
+    print( fnA(7,8,9) ) # three arguments
     fnA(4,3,2, True, fnA, 'clever', {}, [], ('A',)) # arbitrary values passed as positional arguments 
+    print( fnB(x=3, n='Floela', e='nonsuch@dooby.ie', fn=fnB, l=[5,4,3,2,1]) )
