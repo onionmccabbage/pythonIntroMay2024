0afec2dcf28a51da71736e49c9315ff04b8eee09
onionmccabbage <onionmccabbage@live.com>
2024-05-30 10:13:27 +0100

    iterating over collections

---
+++ using_collections/using_iteration.py
@@ -0,0 +1,20 @@
+# here we will build tuple, list and dict object then iterate over members
+
+l = []
+k = list(('Ethel', 'Deidre', 'Enit', 'Fran')) # we put a tuple into our list
+for i in k:
+    print(i)
+
+m = (4,) # a one-member tuple - careful dont just write (4)
+n = (4, 6, 20) # here we have a tuple
+for i in n:
+    print(i)
+t = tuple() # an empty tuple - very pointless!!
+
+d = dict() # explicit dict
+e = {'n':k[0], 'age':n[2], 'recent':True, 'gender':'cis'}
+# we can iterate over a dict
+for (k,v) in e.items: # by convention (k,v) for key,value
+    print(f'key {k} has value {v}')
+
+s = set()
