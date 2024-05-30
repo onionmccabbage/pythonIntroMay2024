c4603a54ec57d7391c1764e3ea07a2de2509b1ab
onionmccabbage <onionmccabbage@live.com>
2024-05-30 11:21:50 +0100

    using comprehension

--- using_collections/using_comprehension.py
+++ using_collections/using_comprehension.py
@@ -1,7 +1,15 @@
 # comprehension means dealing comprehenively with every member of a collection
 from using_iteration import n as my_tuple, k as my_list, e as my_dict
 
+def findCaracterFreq(s):
+    '''count the frequency of every character in a string'''
+    char_d = {letter:s.count(letter) for letter in s}
+    return char_d
+
 if __name__ == '__main__':
-    t = [n for n in my_tuple] # comprehension
-    print(t, type(t))
+    t = [n for n in my_tuple] # tuple comprehension
+    s = [n for n in my_list]  # list comprehension
+    r = [n*n for n in range(0,11)] # range comrehension
+    print(r, type(r))
+    print( findCaracterFreq('how many of each letter do we have?') )
 
