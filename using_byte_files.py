8872d91cc6ffc81c33179beebf9f304c42791f6d
onionmccabbage <onionmccabbage@live.com>
2024-05-30 12:08:19 +0100

    making and writing bytes

---
+++ using_byte_files.py
@@ -0,0 +1,21 @@
+def makeBytes(s):
+    '''convert a string into bytes'''
+    b = s.encode() # encode will convert any string into bytes
+    return b # remember to return the bytes
+
+def makeText(b):
+    '''convert bytes to text'''
+    t = b.decode()
+    return t
+
+def writeBytes(b):
+    '''commit bytes to a file'''
+    try:
+        with open('my_bytes', 'ab') as fout: # 'ab' will append bytes
+            fout.write(b)
+    except Exception as err:
+        print(f'problem {err}')
+
+if __name__ == '__main__':
+    b = makeBytes('here is plain text\n')
+    writeBytes(b)
